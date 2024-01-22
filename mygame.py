988022376a348d5dadf858479a315904a6dd9043
Karita <karitalainee@gmail.com>
2024-01-22 09:32:38 +0200

    Tuntimuistiinpanoja

--- mygame.py
+++ mygame.py
@@ -1,29 +1,25 @@
 import pygame
-import sys     # sys-module will be needed to exit the game
-from pygame.locals import * # imports the constants of pygame
-pygame.init()  # initializes pygame
+import sys     # Mahdollistaa pelin sulkemisen
+from pygame.locals import * # Tuottaa mahdolliset muuttujat pygameen
+pygame.init()  # Ottaa pygamen käyttöön
 
-# the display surface
+# Ikkunan koko
 width = 960
 height = 600
 dispSurf = pygame.display.set_mode((width,height))
-pygame.display.set_caption("My game")
+pygame.display.set_caption("My game") # Ikkunan nimen vaihto
+pygame.display.set_icon(pygame.image.load("mario.png")) # Ikkunan kuvan vaihto
 
-# the Surface objects
+# Objektit
 level = pygame.image.load("level.jpg").convert()
 mario = pygame.image.load("mario.png").convert()
 fireball = pygame.image.load("fireball.png").convert()
-# pygame.image.load(file) function loads a picture "file" into a given variable
-# convert() method converts the picture into the right pixel-format
-# picture files needs to be in the same folder as this python file
-# the folder path can be relative or absolute:
-# relative path: mario = pygame.image.load("folder\\mario.png").convert()
-# absolute path: fireball = pygame.image.load("C:\\folder\\fireball.png").convert()
+# Convert muuttaa kuvan oikeaan muotoon
 
-# empty black Surface(width, height)
+# Suorakulmion arvot, musta (width, height)
 rectangle = pygame.Surface((300,50))
 
-# RGB-colors are tuples (r,g,b), where 0<r,g,b<255
+# RGB värejä, 0-255
 black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
@@ -31,18 +27,16 @@ green = (0,255,0)
 blue = (0,0,255)
 pink = (255,0,130)
 
-# Surface objects can be filled with a color using fill() method
+# Asetetaan suorakulmiolle väri
 rectangle.fill(pink)
 
-# Surface objects can be added to the display surface with blit() method
-# blit(Surface,(x,y)) adds "Surface" into coordinates (x,y)=(left, top)
+# Asetetaan objektit tiettyihin koordinaatteihin
 dispSurf.blit(level, (0,0))
 dispSurf.blit(fireball, (0,0))
-dispSurf.blit(mario, (400,500))
-dispSurf.blit(rectangle, (0,200))
+dispSurf.blit(mario, (400,450)) # 400p oikealle, 450p alas
+dispSurf.blit(rectangle, (0,200)) # Random suorakulmio?
 
-# the display surface needs to be updated for the blitted Surfaces to become visible
-# pygame.display.update() would do the same
+# Päivittää muutokset, myös pygame.display.update()
 pygame.display.flip()
 
 # Surface.get_rect() method returns the Rect object of "Surface"
@@ -56,30 +50,29 @@ rectangleArea = rectangle.get_rect()
 # mario and rectangle were not blitted into (0,0)
 # the left and top coordinates have to be changed with dot notation
 marioArea.left = 400
-marioArea.top = 500
+marioArea.top = 450
 rectangleArea.left = 0
 rectangleArea.top = 200
 
-# speed contains the [x,y]-speed of the fireball in pixels
+# Tulipallon [x, y] nopeus pikseleinä per iteraatio
 speed = [1,1]
 
 
 
-
-
-# the game loop which runs until sys.exit()
+# Looppi pyörii, kunnes pelaaja sulkee pelin
 while True:
 
 
-    # loop to check if the user has closed the display window or pressed esc
-    for event in pygame.event.get():  # list of all the events in the event queue
-        if event.type == pygame.QUIT: # if the player closed the window
-            pygame.quit() # the display window closes
-            sys.exit()    # the python program exits
-        if event.type == KEYDOWN:     # if the player pressed down any key
-            if event.key == K_ESCAPE: # if the key was esc
-                pygame.quit() # the display window closes
-                sys.exit()    # the python program exits
+    # Käy läpi kaikki tapahtumat (quit, exit)
+    for event in pygame.event.get():  # Lista kaikista eventeistä (tapahtumista)
+        if event.type == pygame.QUIT: # Jos pelaaja sulkee ikkunan
+            pygame.quit() # Ikkuna sulkeutuu
+            sys.exit()    # Python ohjelma sulkeutuu
+        if event.type == KEYDOWN:     # Jos käyttäjä painaa mitä tahansa näppäintä
+            if event.key == K_ESCAPE: # Jos näppäin on ESC,
+                                      # Lista näppäimistä löytyy pygame.key
+                pygame.quit() # Ikkuna sulkeutuu
+                sys.exit()    # Python ohjelma sulkeutuu
 
 
     # fireball will be moved by speed=[1,1] in every iteration
@@ -94,7 +87,7 @@ while True:
         speed[1] = -speed[1] # the y-direction of the speed will be converted
 
 
-    # fireball bounces from the rectangle
+    # Fireball törmää suorakulmioon
     if rectangleArea.colliderect(fireballArea):
     # a.colliderect(b) returns True if Rect-objects a and b overlap
         if rectangleArea.colliderect(fireballArea.move(-speed[0],0)):
@@ -105,11 +98,11 @@ while True:
             speed[0] = -speed[0] # the x-direction of the speed will be converted
 
 
-    # mario can be moved with left/right/up/down-keys
-    # get.pressed() function gives a boolean list of all the keys if they are being pressed
+    # Mario liikkuu nuolinäppäimien avulla
+    # get.pressed() listaa kaikki painetut näppäimet, monitoroi näppäimistön toimintaa
     pressings = pygame.key.get_pressed()
-    if pressings[K_LEFT]:          # if left-key is true in the list
-        marioArea.move_ip((-1,0))  # mario will be moved one pixel left
+    if pressings[K_LEFT]:          # Jos pelaaja painaa vasenta nuolinäppäintä
+        marioArea.move_ip((-1,0))  # Mario liikkuu yhden pikselin vasemmalle
     if pressings[K_RIGHT]:
         marioArea.move_ip((1,0))
     if pressings[K_DOWN]:
@@ -118,8 +111,9 @@ while True:
         marioArea.move_ip((0,-1))
 
 
-    # blit all the Surfaces in their new places
-    dispSurf.blit(level, (0,0)) # without this, moving characters would have a "trace"
+    # Päivitä objektit paikoilleen
+    dispSurf.blit(level, (0,0)) # Ilman tätä liikkuvat objektit jättäisivät "jäljen"
+                                # eli mustan vanan peräänsä
     dispSurf.blit(fireball, fireballArea)
     dispSurf.blit(mario, marioArea)
     dispSurf.blit(rectangle, rectangleArea)
@@ -127,4 +121,3 @@ while True:
 
     # updating the display surface is always needed at the end of each iteration of game loop
     pygame.display.flip()
-
