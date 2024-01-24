ef8e721bdca362e9792b4e9ecef7e0e102f408a2
KaritaLaine <44746@edu.raseko.fi>
2024-01-24 20:49:31 +0200

    Matikan kirjan väistelypeli

--- mygameA4.py
+++ mygameA4.py
@@ -5,51 +5,52 @@ pygame.init()
 
 
 # Näyttö
-korkeus = 600
-leveys = 800
+korkeus = 700
+leveys = 900
 naytto = pygame.display.set_mode((leveys, korkeus))
 pygame.display.set_caption("A4 Peli")
 
 # RGB värit
 valkoinen = (255,255,255)
-vihrea = (0,80,0)
+harmaa = (128,128,128)
 musta = (0,0,0)
-
 # Taustaväri
 naytto.fill(valkoinen)
 
-# Objektit
+# Ladattavat objektit
 hahmo = pygame.image.load("hahmo.jpg").convert()
-matikka = pygame.image.load("matikkaI.jpg").convert()
-
+matikka = pygame.image.load("matikka.jpg").convert()
 
 # Taso
-platform = pygame.Surface((leveys, 20))  
-platform.fill(vihrea)  # Täytetään taso vihreällä
+taso = pygame.Surface((leveys, 80))  
+taso.fill(harmaa)  # Täytetään taso vihreällä
 
 # Asetetaan taso peliruudun alareunaan
-platform_rect = platform.get_rect()
-platform_rect.bottom = korkeus
+taso_rect = taso.get_rect()
+taso_rect.bottom = korkeus
 
 # Asetetaan teksti peliin
 font = pygame.font.SysFont('Arial', 25) # Fontti
-teksti = font.render('Mission: Survive', True, musta)  # Teksti ja väri
-teksti_rect = teksti.get_rect(center=(leveys//2, korkeus//8))  # Positio
+teksti = font.render('Mission: Survive', True, musta)  # Teksti ja väria
+teksti_rect = teksti.get_rect(center=(leveys//2, korkeus//5.5))  # Positiod
 
+# Asetetaan kirja peliin
+matikka_rect = matikka.get_rect()
+
+# Matikan nopeus
+m_nopeus = [10,20]
 
 # Ajastin
 aikaaJaljella = 60
-
-timer_text = font.render('Time: ' + str(aikaaJaljella), True, musta)  # Update timer text
-timer_rect = timer_text.get_rect(center=(leveys//2, korkeus//4))
-
+ajastinTeksti = font.render('Time: ' + str(aikaaJaljella), True, musta)  # Update timer texta
+ajastin_rect = ajastinTeksti.get_rect(center=(leveys//2, korkeus//4))
 aloitusAika = pygame.time.get_ticks()
 
-
-# Hahmon nopeus
-speed = 5
+# Hahmon nopeus ja positio
+nopeus = 5
 x = leveys // 2  # Hahmon X-positio
-y = korkeus - hahmo.get_height() - platform.get_height()  #  Hahmon Y-positio
+y = korkeus - hahmo.get_height() - taso.get_height()  #  Hahmon Y-positio
+
 
 
 # Pelilogiikka, pyörii kunnes käyttäjä painaa ESC-näppäintä
@@ -65,7 +66,7 @@ while True:
                 sys.exit()
     
     # Pelaaja painaa näppäintä
-    keys = pygame.key.get_pressed()
+    nappaimet = pygame.key.get_pressed()
 
     aikaaJaljella = pygame.time.get_ticks() - aloitusAika
     aikaaJaljella = max(0, 60 - int(aikaaJaljella / 1000))
@@ -75,25 +76,53 @@ while True:
         sys.exit() 
     
     # Hahmo liikkuu näppäimen mukaan
-    if keys[pygame.K_a]:  # Jos painetaan A
-        x -= speed  # hahmo liikkuu vasemmalle
-    if keys[pygame.K_d]:  # Jos painetaan D
-        x += speed  # Hahmo liikkuu oikealle
+    if nappaimet[pygame.K_a]:  # Jos painetaan A
+        x -= nopeus  # hahmo liikkuu vasemmalle
+    if nappaimet[pygame.K_d]:  # Jos painetaan D
+        x += nopeus  # Hahmo liikkuu oikealle
+        
+        
+    # Matikan kirja liikkuu joka iteraatiolla
+    matikka_rect.move_ip(m_nopeus)
+    
+    # Matikan kirja kimpoaa seinistä kun se yrittää mennä ikkunan ulkopuolelle
+    if matikka_rect.left < 0 or matikka_rect.right > leveys: 
+        m_nopeus[0] = -m_nopeus[0] 
+    if matikka_rect.top < 0 or matikka_rect.bottom > korkeus: 
+        m_nopeus[1] = -m_nopeus[1] 
 
-    # Varmistetaan, että hahmo pysyy platformin päällä
+
+    # Matikan kirja kimpoaa lattiasta
+    if taso_rect.colliderect(matikka_rect):
+    # Kun matikan kirja osuu taso-alueeseen, se kimpoaa vastakkaiseen suuntaan
+        if taso_rect.colliderect(matikka_rect.move(-m_nopeus[0],0)):
+            m_nopeus[1] = -m_nopeus[1]
+        else:
+            m_nopeus[0] = -m_nopeus[0]
+            
+    # Varmistetaan, että hahmo pysyy tasoin päällä
     x = max(0, min(leveys - hahmo.get_width(), x))  
+
+    # Hahmon positio
+    hahmo_rect = hahmo.get_rect(topleft=(x, y))
+
+    # Tarkistetaan osuuko hahmo kirjaan
+    if hahmo_rect.colliderect(matikka_rect):
+        print("Hävisit pelin!")
+        pygame.quit()
+        sys.exit()
+        
+    # Päivitetään ajastimen tekstiä
+    ajastinTeksti = font.render('Time: ' + str(aikaaJaljella), True, musta)  # Päivitetään timerin tekstiä
 
     # Blitataan opjektit näytölle
     naytto.fill(valkoinen) 
-    naytto.blit(platform, platform_rect)
     naytto.blit(hahmo, (x, y))
+    naytto.blit(taso, taso_rect)
     naytto.blit(teksti, teksti_rect)
-    
-    naytto.fill(valkoinen, timer_rect)  # Täytetään timerin alue valkoisella
-    timer_text = font.render('Time: ' + str(aikaaJaljella), True, musta)  # Päivitetään timerin tekstiä
-    naytto.blit(timer_text, timer_rect)  # Laitetaan päivitetty teksti näytölle
-    
-
+    naytto.fill(valkoinen, ajastin_rect)  # Täytetään timerin alue valkoisella
+    naytto.blit(ajastinTeksti, ajastin_rect)  # Laitetaan päivitetty teksti näytölle
+    naytto.blit(matikka, matikka_rect)
 
     # Päivitys
     pygame.display.update()
